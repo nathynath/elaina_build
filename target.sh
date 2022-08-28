c14e1130bee4786a7c40a924fab4fe8e95bc8fc4
nathynath <106421743+nathynath@users.noreply.github.com>
2022-08-28 19:48:30 +0700

    Update target.sh

--- target.sh
+++ target.sh
@@ -3,8 +3,8 @@
 # Copyright (C) 2021 a xyzprjkt property
 
 # Main
-KERNEL_ROOTDIR= # IMPORTANT ! Fill with your kernel source root directory.
-DEVICE_DEFCONFIG= # IMPORTANT ! Declare your kernel source defconfig file here.
-CLANG_ROOTDIR= # IMPORTANT! Put your clang directory here.
-export KBUILD_BUILD_USER= # Change with your own name or else.
-export KBUILD_BUILD_HOST= # Change with your own hostname.
+KERNEL_ROOTDIR=/workspace/Gitpod-Workspaces/katzlein # IMPORTANT ! Fill with your kernel source root directory.
+DEVICE_DEFCONFIG=vendor/katzlein-perf_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
+CLANG_ROOTDIR=/workspace/Gitpod-Workspaces/proton # IMPORTANT! Put your clang directory here.
+export KBUILD_BUILD_USER=ynthanns # Change with your own name or else.
+export KBUILD_BUILD_HOST=nathynath # Change with your own hostname.
