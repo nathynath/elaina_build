96d4d8a7965992afc0d16d66d94fee5971c962b8
nathynath <106421743+nathynath@users.noreply.github.com>
2022-09-18 09:53:06 +0700

    target.sh: Change Katzlein to Elaina

--- target.sh
+++ target.sh
@@ -3,8 +3,8 @@
 # Copyright (C) 2021 a xyzprjkt property
 
 # Main
-KERNEL_ROOTDIR=/workspace/Gitpod-Workspaces/katzlein # IMPORTANT ! Fill with your kernel source root directory.
-DEVICE_DEFCONFIG=vendor/katzlein-perf_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
+KERNEL_ROOTDIR=/workspace/Gitpod-Workspaces/elaina # IMPORTANT ! Fill with your kernel source root directory.
+DEVICE_DEFCONFIG=vendor/elaina-perf_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
 CLANG_ROOTDIR=/workspace/Gitpod-Workspaces/proton # IMPORTANT! Put your clang directory here.
 export KBUILD_BUILD_USER=ynthanns # Change with your own name or else.
 export KBUILD_BUILD_HOST=nathynath # Change with your own hostname.
